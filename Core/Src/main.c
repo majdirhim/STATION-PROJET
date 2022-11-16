38db3a08e09119487b069330440839757398f838
Farah BALEHOUANE <balehouane.farah@yahoo.com>
2022-11-16 14:42:50 +0100

    second commit

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -18,8 +18,13 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "i2c.h"//Mode de communication I2C
+#include "usart.h"
 #include "gpio.h"
-
+#include "lps22hh_reg.h"
+#include "string.h"
+#include "stdio.h"
+#include "stm32f7xx_hal.h"
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 
@@ -36,18 +41,37 @@
 
 /* Private macro -------------------------------------------------------------*/
 /* USER CODE BEGIN PM */
-
+int _write(int file, char *ptr, int len)//surcharge la focntion pour printf
+{
+	HAL_UART_Transmit(&huart1,(uint8_t*)ptr,len,100);
+	return len;
+}
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
 
 /* USER CODE BEGIN PV */
 
+static uint32_t data_raw_pressure;
+static int16_t data_raw_temperature;
+static float pressure_hPa;
+static float temperature_degC;
+static uint8_t whoamI, rst;
+static uint8_t tx_buffer[1000];
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 /* USER CODE BEGIN PFP */
+
+
+static int32_t platform_write(void *handle, uint8_t reg, const uint8_t *bufp,
+                             uint16_t len);
+static int32_t platform_read(void *handle, uint8_t reg, uint8_t *bufp,
+                            uint16_t len);
+static void tx_com( uint8_t *tx_buffer, uint16_t len );
+static void platform_delay(uint32_t ms);
+static void platform_init(void);
 
 /* USER CODE END PFP */
 
@@ -64,6 +88,14 @@ int main(void)
 {
   /* USER CODE BEGIN 1 */
 
+	 stmdev_ctx_t dev_ctx;
+	  	  lps22hh_reg_t reg;
+	  	  /* Initialize mems driver interface */
+	  	  dev_ctx.write_reg = platform_write;
+	  	  dev_ctx.read_reg = platform_read;
+	  	  dev_ctx.handle = &hi2c1;//c'est un pointeur pour la communication I2C
+
+
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
@@ -72,7 +104,7 @@ int main(void)
   HAL_Init();
 
   /* USER CODE BEGIN Init */
-
+  platform_init();
   /* USER CODE END Init */
 
   /* Configure the system clock */
@@ -84,7 +116,33 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
+  MX_I2C1_Init();
+  MX_USART1_UART_Init();
+
   /* USER CODE BEGIN 2 */
+
+  	  /* Initialize platform specific hardware */
+
+  	  /* Wait sensor boot time */
+  	  platform_delay(5);
+  	  /* Check device ID */
+  	  whoamI = 0;//C'est un registre pour la communication I2C
+  	  lps22hh_device_id_get(&dev_ctx, &whoamI);//Ce trouve dans le reg.c
+
+  	  if ( whoamI != LPS22HH_ID )
+  	    while (1); /*manage here device not found */
+
+  	  /* Restore default configuration */
+  	  lps22hh_reset_set(&dev_ctx, PROPERTY_ENABLE);
+
+  	  do {//Reset du capteur je pense
+  	    lps22hh_reset_get(&dev_ctx, &rst);
+  	     } while (rst);
+
+  	  /* Enable Block Data Update */
+  	  lps22hh_block_data_update_set(&dev_ctx, PROPERTY_ENABLE);
+  	  /* Set Output Data Rate */
+  	  lps22hh_data_rate_set(&dev_ctx, LPS22HH_10_Hz_LOW_NOISE);
 
   /* USER CODE END 2 */
 
@@ -92,12 +150,35 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+	  lps22hh_read_reg(&dev_ctx, LPS22HH_STATUS, (uint8_t *)&reg, 1);//etat du capteur
+
+  if (reg.status.p_da) {
+    memset(&data_raw_pressure, 0x00, sizeof(uint32_t));//initialisation la valeur  et la mettre dans le data raw pressure
+    //(memset and lps22hrawget
+    lps22hh_pressure_raw_get(&dev_ctx, &data_raw_pressure);//lire la donnée
+    pressure_hPa = lps22hh_from_lsb_to_hpa( data_raw_pressure);//hp..... qui de int a float
+    printf( "La pression mesurée est [hPa]:%6.2f\r\n", pressure_hPa);
+
+  }
+
+  if (reg.status.t_da) {
+    memset(&data_raw_temperature, 0x00, sizeof(int16_t));
+    lps22hh_temperature_raw_get(&dev_ctx, &data_raw_temperature);
+    temperature_degC = lps22hh_from_lsb_to_celsius(
+                         data_raw_temperature );
+    printf( "La temperature mesurée [degC]:%3.2f\r\n", temperature_degC );
+
+
+  }
+  HAL_Delay(1000);//1 seconde
+}
     /* USER CODE END WHILE */
-temp
+
     /* USER CODE BEGIN 3 */
   }
+
   /* USER CODE END 3 */
-}
+
 
 /**
   * @brief System Clock Configuration
@@ -111,17 +192,18 @@ void SystemClock_Config(void)
   /** Configure the main internal regulator output voltage
   */
   __HAL_RCC_PWR_CLK_ENABLE();
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
   */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLM = 12;
-  RCC_OscInitStruct.PLL.PLLN = 96;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 200;
   RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
   RCC_OscInitStruct.PLL.PLLQ = 2;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
@@ -142,17 +224,84 @@ void SystemClock_Config(void)
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_3) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_6) != HAL_OK)
   {
     Error_Handler();
   }
 }
+//poiteur de fonction
 
 /* USER CODE BEGIN 4 */
+static int32_t platform_write(void *handle, uint8_t reg, const uint8_t *bufp,
+                              uint16_t len)
+{
+  HAL_I2C_Mem_Write(&hi2c1, LPS22HH_I2C_ADD_H, reg,
+                    I2C_MEMADD_SIZE_8BIT, (uint8_t*) bufp, len, 1000);
 
+  return 0;
+}
+
+/*
+ * @brief  Read generic device register (platform dependent)
+ *
+ * @param  handle    customizable argument. In this examples is used in
+ *                   order to select the correct sensor bus handler.
+ * @param  reg       register to read
+ * @param  bufp      pointer to buffer that store the data read
+ * @param  len       number of consecutive register to read
+ *
+ */
+static int32_t platform_read(void *handle, uint8_t reg, uint8_t *bufp,
+                             uint16_t len)
+{
+  HAL_I2C_Mem_Read(&hi2c1, LPS22HH_I2C_ADD_H, reg,
+                   I2C_MEMADD_SIZE_8BIT, bufp, len, 1000);
+  return 0;
+}
+
+/*
+ * @brief  Write generic device register (platform dependent)
+ *
+ * @param  tx_buffer     buffer to transmit
+ * @param  len           number of byte to send
+ *
+ */
+static void tx_com(uint8_t *tx_buffer, uint16_t len)
+{
+
+  HAL_UART_Transmit(&huart1, tx_buffer, len, 1000);//pour afficher
+
+}
+
+/*
+ * @brief  platform specific delay (platform dependent)
+ *
+ * @param  ms        delay in ms
+ *
+ */
+static void platform_delay(uint32_t ms)
+{
+
+  HAL_Delay(ms);
+
+}
+
+/*
+ * @brief  platform specific initialization (platform dependent)
+ */
+static void platform_init(void)
+{
+#if defined(STEVAL_MKI109V3)
+  TIM3->CCR1 = PWM_3V3;
+  TIM3->CCR2 = PWM_3V3;
+  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_1);
+  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_2);
+  HAL_Delay(1000);
+#endif
+}
 /* USER CODE END 4 */
 
 /**
