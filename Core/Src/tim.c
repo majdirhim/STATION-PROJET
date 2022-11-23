88bd85a24ff3ec9cde4ace5570ddc348cfabd727
majdirhim <54720363+majdirhim@users.noreply.github.com>
2022-11-23 11:21:33 +0100

    pullup Timer

--- Core/Src/tim.c
+++ Core/Src/tim.c
@@ -134,7 +134,7 @@ void HAL_TIM_Base_MspInit(TIM_HandleTypeDef* tim_baseHandle)
     */
     GPIO_InitStruct.Pin = GPIO_PIN_8;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Pull = GPIO_PULLUP;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
     GPIO_InitStruct.Alternate = GPIO_AF1_TIM1;
     HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
